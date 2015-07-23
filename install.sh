7b780165a0449d0589eabb6ec0c29f0ae2830c81
Jamie York <ziadoz@gmail.com>
2015-07-23 12:49:42 +0100

    Added more comments to install

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 # To Do
 # - Install Home Brew
-# - Install Home Brew dependencies: ffmpeg, git, mercurial, svn, wget, bash completion, imagemagick, rbenv, ruby-build, tree, ansible 
+# - Install Home Brew dependencies: 
+# bash, bash completion, zsh
+# git, mercurial, svn
+# rbenv, ruby-build
+# ffmpeg, imagemagick, wget, tree, ansible, ncdu, mosh
+# brew tap josegonzalez/php
+
+# Resources: 
+# http://apple.stackexchange.com/questions/12161/os-x-terminal-must-have-utilities
+# https://github.com/Bash-it/bash-it
+# http://ohmyz.sh/
