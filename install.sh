411bf880bdaf6bbe016977a0278a504ccc48bfce
Jamie York <Jamie@Barricade.local>
2016-12-28 16:34:42 +0000

    Check before installing brew, cask and ansible. Remove sudo prompt.

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-sudo -v
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+if ! which brew > /dev/null; then
+    echo 'Installing Homebrew: '
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-echo 'Installing Homebrew: '
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! brew info cask &>/dev/null; then
+  echo 'Installing Cask: '
+  brew tap caskroom/cask
+fi
 
-echo 'Installing Cask: '
-brew tap caskroom/cask
-
-echo 'Installing Ansible'
-brew install ansible
+if ! which ansible > /dev/null; then
+  echo 'Installing Ansible'
+  brew install ansible
+fi
 
 echo 'Running Ansible Playbook'
 ansible-playbook -i ansible/inventory --ask-sudo-pass ansible/playbook.yml
