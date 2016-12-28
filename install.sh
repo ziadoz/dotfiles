e7d76724617235963a4aa95ec55103b2a4afe569
Jamie <ziadoz@gmail.com>
2016-12-28 15:41:30 +0000

    Add Cask and Caffiene application

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 echo 'Installing Homebrew: '
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo 'Installing Cask: '
+brew tap caskroom/cask
+
 echo 'Installing Ansible'
 brew install ansible
 
