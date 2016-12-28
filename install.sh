98f55f18decb5c7771c34570eb72608930fdcf3a
Jamie <ziadoz@gmail.com>
2016-12-28 00:58:40 +0000

    Start fleshing out Ansible playbook

--- install.sh
+++ install.sh
@@ -1,34 +1,18 @@
 #!/usr/bin/env bash
 
-# Ask for password upfront
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 echo 'Installing Homebrew: '
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
-brew upgrade --all
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo 'Installing Shell Enhancements: '
-brew install bash-completion
-
-echo 'Installing VCS: '
-brew install git mercurial svn
-
-echo 'Installing Ansible: '
+echo 'Installing Ansible'
 brew install ansible
 
-echo 'Installing Languages: '
-brew ruby-build rbenv
+echo 'Running Ansible Playbook'
+ansible-playbook -i ../ansible/inventory ../ansible/playbook.yml
 
-echo 'Installing Shell Utilities: '
-brew install wget --with-iri
-brew install tree ncdu mosh archey screenfetch ack p7zip
-
-echo 'Installing Utilities: '
-brew install httpie sleepwatcher
-
-echo 'Cleaning Up '
+echo 'Cleaning Up'
 brew cleanup
 brew prune
 
