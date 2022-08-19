2e2cbf9d66e2216726dddd8e43ae132a8b1b1820
Jamie <ziadoz@gmail.com>
2022-08-19 11:19:31 +0100

    remove old bash scripts

--- ansible/roles/shell/files/Microsoft.PowerShell_profile.ps1
+++
@@ -1,1 +0,0 @@
-Function WSL-SetDefaultUser ($distro, $user) { Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq $distro | Set-ItemProperty -Name DefaultUid -Value ((wsl -d $distro -u $user -e id -u) | Out-String); };
